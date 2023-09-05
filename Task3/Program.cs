6c3dd31545bbe69c8256371cc78524e7f5e530b9
Bronivick <mister.galiakhmetov2016@yandex.ru>
2023-09-06 00:41:10 +0300

    add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿int blyat = new Random().Next(1,8);
+
+Console.WriteLine(blyat);
+
+int blyat1 = blyat % 10;
+
+int result = blyat1;
+
+if(blyat>5)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
