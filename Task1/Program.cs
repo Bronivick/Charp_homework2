e1209627a6863f9a55dbcabefacceffae1d64de0
Bronivick <mister.galiakhmetov2016@yandex.ru>
2023-09-06 00:07:58 +0300

    add task1

---
+++ Task1/Program.cs
@@ -0,0 +1,10 @@
+﻿int blyat = new Random().Next(100,1000);
+
+Console.WriteLine(blyat);
+
+int blyat1 = blyat % 10;
+int blyat2 = blyat % 100;
+int result = (blyat2-blyat1)/10;
+
+Console.WriteLine(result);
+
