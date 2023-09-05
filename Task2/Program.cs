d64fdf742e296b9a9c5df5cf941c9de7967aec7d
Bronivick <mister.galiakhmetov2016@yandex.ru>
2023-09-06 00:30:10 +0300

    add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿int blyat = new Random().Next(45,1000);
+
+Console.WriteLine(blyat);
+
+int blyat1 = blyat % 10;
+
+int result = blyat1;
+
+if(blyat>100)
+{
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("третьей цифры нету");
+}
